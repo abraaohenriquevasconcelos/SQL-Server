cfd036cd14fa4c0793f11a8ea17c6b362bc6d5f1
Abraão Henrique Vasconcelos <henrique.vscncls@gmail.com>
2019-05-12 20:04:12 -0300

    Add files via upload

---
+++ SQLQuery2.sql
@@ -0,0 +1,47 @@
+
+/* de todos os objetos*/
+select SCHEMA_NAME(schema_id) + '.' + name as table_view,
+type, type_desc, create_date,  modify_date 
+from sys.objects 
+where type = 'U' and name like '%PersonPhone%'
+
+select * from sys.tables where name like '%PersonPhone%' --Do próprio banco
+EXEC sys.sp_msforeachdb  'SELECT ''?'' DatabaseName, Name FROM [?].sys.tables WHERE Name LIKE ''%PersonPhone%'''
+
+
+
+
+
+Person.PersonPhone
+
+select top 1 * from sys.objects
+
+
+	select schema_name(t.schema_id) + '.' + t.[name] as table_view, 
+        case when t.[type] = 'U' then 'Table'
+            when t.[type] = 'V' then 'View'
+            end as [object_type],
+        case when c.[type] = 'PK' then 'Primary key'
+            when c.[type] = 'UQ' then 'Unique constraint'
+            when i.[type] = 1 then 'Unique clustered index'
+            when i.type = 2 then 'Unique index'
+            end as constraint_type, 
+        isnull(c.[name], i.[name]) as constraint_name,
+        substring(column_names, 1, len(column_names)-1) as [details]
+    from sys.objects t
+        left outer join sys.indexes i
+            on t.object_id = i.object_id
+        left outer join sys.key_constraints c
+            on i.object_id = c.parent_object_id 
+            and i.index_id = c.unique_index_id
+       cross apply (select col.[name] + ', '
+                        from sys.index_columns ic
+                            inner join sys.columns col
+                                on ic.object_id = col.object_id
+                                and ic.column_id = col.column_id
+                        where ic.object_id = t.object_id
+                            and ic.index_id = i.index_id
+                                order by col.column_id
+                                for xml path ('') ) D (column_names)
+
+
